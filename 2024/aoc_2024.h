e4bbed445720ed61c7f9936770b7c95cca187a5f
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-12-05 16:11:11 +0100

    init: 2024 challenge

---
+++ 2024/aoc_2024.h
@@ -0,0 +1,19 @@
+#ifndef _AOC_2024_H_
+#define _AOC_2024_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+
+#define AOC_YEAR 2024
+
+#ifdef TEST
+#define INPUT_FILE "tests/test.txt"
+#else
+#define INPUT_FILE "tests/input.txt"
+#endif
+
+#endif /* _AOC_2024_H_ */
