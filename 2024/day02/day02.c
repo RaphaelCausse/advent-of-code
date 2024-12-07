c371add44eb5bf4e7b1c2c93779b9984c42a44d1
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-12-08 00:39:12 +0100

    add: 2024 - day 2 - part 1 (success)

--- 2024/day02/day02.c
+++ 2024/day02/day02.c
@@ -12,13 +12,25 @@
 
 #define AOC_DAY 2
 
+#define NUM_LEVEL 100
+
+#define DIFF_MIN 1
+#define DIFF_MAX 3
+
 /***** Globals ***************************************************************/
 
 /***** Functions *************************************************************/
 
-err_t read_input(void)
+err_t solve_part_one(void)
 {
     FILE *input_file;
+    int32_t levels[NUM_LEVEL] = {0};
+    size_t num_levels = 0;
+    uint32_t count_safe_reports = 0;
+    int32_t diff = 0;
+    uint8_t num_pos = 0;
+    uint8_t num_neg = 0;
+    bool safe_report = false;
 
     input_file = fopen(INPUT_FILE, "r");
     if (input_file == NULL)
@@ -27,20 +39,63 @@ err_t read_input(void)
         return E_ERROR;
     }
 
-    /* TODO */
+    while (!feof(input_file))
+    {
+        num_levels = 0;
+        while (fscanf(input_file, "%d", &levels[num_levels]) == 1)
+        {
+            num_levels++;
+            if (getc(input_file) == '\n')
+            {
+                break;
+            }
+        }
+
+        if (num_levels < 2)
+        {
+            continue;
+        }
+
+        num_pos = 0;
+        num_neg = 0;
+        safe_report = true;
+
+        for (size_t i = 0; i < num_levels - 1; i++)
+        {
+            diff = levels[i] - levels[i + 1];
+            if (diff > 0)
+            {
+                num_pos++;
+            }
+            else if (diff < 0)
+            {
+                num_neg++;
+            }
+
+            if (abs(diff) < DIFF_MIN || abs(diff) > DIFF_MAX)
+            {
+                safe_report = false;
+                break;
+            }
+        }
+        if (!(num_pos == 0 || num_neg == 0))
+        {
+            safe_report = false;
+        }
+
+        if (safe_report)
+        {
+            count_safe_reports++;
+        }
+    }
+
+    printf("output: %u\n", count_safe_reports);
 
     if (fclose(input_file) != 0)
     {
         printf("ERROR\t%s: %s: (%d) %s\n", __func__, INPUT_FILE, errno, strerror(errno));
         return E_ERROR;
     }
-
-    return E_OK;
-}
-
-err_t solve_part_one(void)
-{
-    /* TODO */
 
     return E_OK;
 }
@@ -54,11 +109,6 @@ err_t solve_part_two(void)
 
 int main(void)
 {
-    if (read_input() == E_ERROR)
-    {
-        return EXIT_FAILURE;
-    }
-
     BANNER_START(AOC_DAY, 1);
     if (solve_part_one() == E_ERROR)
     {
