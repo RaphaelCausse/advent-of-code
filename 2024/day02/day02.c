50f234d7d3d0524f6f7a49c42effedb280759847
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-12-08 15:38:51 +0100

    refactor: 2024 - day 1 - part 1

--- 2024/day02/day02.c
+++ 2024/day02/day02.c
@@ -12,25 +12,24 @@
 
 #define AOC_DAY 2
 
-#define NUM_LEVEL 32
+#define REPORTS_MAX 1024
+#define LEVELS_MAX 16
 
 #define DIFF_MIN 1
 #define DIFF_MAX 3
 
 /***** Globals ***************************************************************/
 
+size_t num_reports = 0;
+int32_t reports[REPORTS_MAX][LEVELS_MAX] = {0};
+size_t report_lengths[REPORTS_MAX] = {0};
+
 /***** Functions *************************************************************/
 
-err_t solve_part_one(void)
+err_t read_input(void)
 {
     FILE *input_file;
-    int32_t levels[NUM_LEVEL] = {0};
     size_t num_levels = 0;
-    uint32_t count_safe_reports = 0;
-    int32_t diff = 0;
-    uint8_t num_pos = 0;
-    uint8_t num_neg = 0;
-    bool safe_report = false;
 
     input_file = fopen(INPUT_FILE, "r");
     if (input_file == NULL)
@@ -42,7 +41,7 @@ err_t solve_part_one(void)
     while (!feof(input_file))
     {
         num_levels = 0;
-        while (fscanf(input_file, "%d", &levels[num_levels]) == 1)
+        while (fscanf(input_file, "%d", &reports[num_reports][num_levels]) == 1)
         {
             num_levels++;
             if (getc(input_file) == '\n')
@@ -51,18 +50,39 @@ err_t solve_part_one(void)
             }
         }
 
-        if (num_levels < 2)
+        if (num_levels >= 2)
         {
-            continue;
+            report_lengths[num_reports] = num_levels;
+            num_reports++;
         }
+    }
 
+    if (fclose(input_file) != 0)
+    {
+        printf("ERROR\t%s: %s: (%d) %s\n", __func__, INPUT_FILE, errno, strerror(errno));
+        return AOC_ERROR;
+    }
+
+    return AOC_OK;
+}
+
+err_t solve_part_one(void)
+{
+    uint32_t count_safe_reports = 0;
+    bool safe_report = true;
+    uint8_t num_pos = 0;
+    uint8_t num_neg = 0;
+
+    for (size_t r = 0; r < num_reports; r++)
+    {
+        safe_report = true;
         num_pos = 0;
         num_neg = 0;
-        safe_report = true;
 
-        for (size_t i = 0; i < num_levels - 1; i++)
+        for (size_t i = 0; i < report_lengths[r] - 1; i++)
         {
-            diff = levels[i] - levels[i + 1];
+            int32_t diff = reports[r][i + 1] - reports[r][i];
+
             if (diff > 0)
             {
                 num_pos++;
@@ -78,6 +98,7 @@ err_t solve_part_one(void)
                 break;
             }
         }
+
         if (!(num_pos == 0 || num_neg == 0))
         {
             safe_report = false;
@@ -91,12 +112,6 @@ err_t solve_part_one(void)
 
     printf("output: %u\n", count_safe_reports);
 
-    if (fclose(input_file) != 0)
-    {
-        printf("ERROR\t%s: %s: (%d) %s\n", __func__, INPUT_FILE, errno, strerror(errno));
-        return AOC_ERROR;
-    }
-
     return AOC_OK;
 }
 
@@ -109,6 +124,11 @@ err_t solve_part_two(void)
 
 int main(void)
 {
+    if (read_input() == AOC_ERROR)
+    {
+        return EXIT_FAILURE;
+    }
+
     BANNER_START(AOC_DAY, 1);
     if (solve_part_one() == AOC_ERROR)
     {
