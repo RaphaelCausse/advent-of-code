fff02a77f8cf24b1f18e66e5746b79a8d6b606fb
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-12-16 23:41:25 +0100

    add: 2024 - day 2 - part 2 (success)

--- 2024/day02/day02.c
+++ 2024/day02/day02.c
@@ -12,8 +12,8 @@
 
 #define AOC_DAY 2
 
-#define REPORTS_MAX 1024
-#define LEVELS_MAX 16
+#define MAX_REPORTS 1024
+#define MAX_LEVELS 16
 
 #define DIFF_MIN 1
 #define DIFF_MAX 3
@@ -21,8 +21,8 @@
 /***** Globals ***************************************************************/
 
 size_t num_reports = 0;
-int32_t reports[REPORTS_MAX][LEVELS_MAX] = {0};
-size_t report_lengths[REPORTS_MAX] = {0};
+int32_t reports[MAX_REPORTS][MAX_LEVELS] = {0};
+size_t report_lengths[MAX_REPORTS] = {0};
 
 /***** Functions *************************************************************/
 
@@ -34,7 +34,7 @@ err_t read_input(void)
     input_file = fopen(INPUT_FILE, "r");
     if (input_file == NULL)
     {
-        printf("ERROR\t%s: %s: (%d) %s\n", __func__, INPUT_FILE, errno, strerror(errno));
+        LOG_ERROR_FILE();
         return AOC_ERROR;
     }
 
@@ -59,14 +59,14 @@ err_t read_input(void)
 
     if (fclose(input_file) != 0)
     {
-        printf("ERROR\t%s: %s: (%d) %s\n", __func__, INPUT_FILE, errno, strerror(errno));
+        LOG_ERROR_FILE();
         return AOC_ERROR;
     }
 
     return AOC_OK;
 }
 
-bool is_safe_report(int32_t levels[], size_t num_levels)
+bool is_report_safe(int32_t levels[], size_t num_levels)
 {
     int32_t diff;
     uint8_t num_pos = 0;
@@ -100,7 +100,7 @@ err_t solve_part_one(void)
 
     for (size_t r = 0; r < num_reports; r++)
     {
-        if (is_safe_report(reports[r], report_lengths[r]))
+        if (is_report_safe(reports[r], report_lengths[r]))
         {
             count_safe_reports++;
         }
@@ -114,19 +114,46 @@ err_t solve_part_one(void)
 err_t solve_part_two(void)
 {
     uint32_t count_safe_reports = 0;
+    bool safe_with_dampener = false;
 
     for (size_t r = 0; r < num_reports; r++)
     {
-        if (is_safe_report(reports[r], report_lengths[r]))
+        if (is_report_safe(reports[r], report_lengths[r]))
+        {
+            count_safe_reports++;
+            continue;
+        }
+
+        /* Problem dampener */
+        safe_with_dampener = false;
+        for (size_t i = 0; i < report_lengths[r]; i++)
+        {
+            int32_t tmp_levels[MAX_LEVELS] = {0};
+            size_t k = 0;
+
+            for (size_t j = 0; j < report_lengths[r]; j++)
+            {
+                if (j != i)
+                {
+                    tmp_levels[k] = reports[r][j];
+                    k++;
+                }
+            }
+
+            if (is_report_safe(tmp_levels, k))
+            {
+                safe_with_dampener = true;
+                break;
+            }
+        }
+
+        if (safe_with_dampener)
         {
             count_safe_reports++;
         }
     }
 
-    /* TODO: Problem Dampener */
-
-    // printf("output: %u\n", count_safe_reports);
-    printf("INFO: solve_part_two not implemented !\n");
+    printf("output: %u\n", count_safe_reports);
 
     return AOC_OK;
 }
